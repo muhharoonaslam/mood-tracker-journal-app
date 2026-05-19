12ae9835021df245a3c761932e52c377ede271c9
haroonaslam <muhharoonaslam@gmail.com>
2026-05-20 04:36:56 +0500

    Update local port configuration from 5000 to 3000 for consistency

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware')
 const path = require('path')
 
 const app = express()
-const PORT = process.env.PORT || 5000
+const PORT = parseInt(process.env.PORT || '3000', 10)
 
 app.use(
   '/api',
